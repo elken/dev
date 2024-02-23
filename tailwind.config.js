b5a841f03c7374472646ff1b6eaac3b93a2e4d2a
Ellis Kenyo <me@elken.dev>
2024-02-23 11:36:15 +0000

    fix: Minor CSS tweaks
    Looks massive thanks to formatting I don't care about, but summarized we
    have:
    
    - ensuring the language name on code blocks isn't interactable
    - ensuring that the JIT is aware of blog posts too

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,55 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: 'class',
-    content: ["./src/**/*.clj"],
-    theme: {
-      extend: {
-        typography: (theme) => ({
-          DEFAULT: {
-            css: {
-              'code::before': {
-                content: 'none'
-              },
-              'code::after': {
-                content: 'none'
-              }
-            }
-          }
-        }),
-        gridTemplateRows: {
-          'page': 'auto 1fr auto'
+  darkMode: "class",
+  content: ["./src/**/*.clj", "./content/**/*.md"],
+  theme: {
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "code::before": {
+              content: "none",
+            },
+            "code::after": {
+              content: "none",
+            },
+          },
         },
-        height: {
-          'responsive-screen': '100svh'
+      }),
+      gridTemplateRows: {
+        page: "auto 1fr auto",
+      },
+      height: {
+        "responsive-screen": "100svh",
+      },
+      spacing: {
+        "responsive-screen": "100svh",
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        nord: {
+          0: "#2E3440",
+          1: "#3B4252",
+          2: "#434C5E",
+          3: "#4C566A",
+          4: "#D8DEE9",
+          5: "#E5E9F0",
+          6: "#ECEFF4",
+          7: "#8FBCBB",
+          8: "#88C0D0",
+          9: "#81A1C1",
+          10: "#5E81AC",
+          11: "#BF616A",
+          12: "#D08770",
+          13: "#EBCB8B",
+          14: "#A3BE8C",
+          15: "#B48EAD",
         },
-        spacing: {
-          'responsive-screen': '100svh'
-        },
-        colors: {
-          transparent: 'transparent',
-          current: 'currentColor',
-          nord: {
-            0: '#2E3440',
-            1: '#3B4252',
-            2: '#434C5E',
-            3: '#4C566A',
-            4: '#D8DEE9',
-            5: '#E5E9F0',
-            6: '#ECEFF4',
-            7: '#8FBCBB',
-            8: '#88C0D0',
-            9: '#81A1C1',
-            10: '#5E81AC',
-            11: '#BF616A',
-            12: '#D08770',
-            13: '#EBCB8B',
-            14: '#A3BE8C',
-            15: '#B48EAD',
-          }
-        }
-      }
+      },
     },
-    plugins: [
-        require('@tailwindcss/typography')
-    ]
-}
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
