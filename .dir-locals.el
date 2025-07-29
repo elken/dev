1637706b453ee19a47c7b7cef2999fb0419c4b7e
Ellis Kenyo <me@elken.dev>
2025-07-29 13:40:37 +0100

    Add .dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((compile-command . "bb dev")
+         (cider-clojure-cli-aliases . "-M:dev"))))
